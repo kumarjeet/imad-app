2baa32af6e828ff6687aa9135a858992857694d3
Jeet Kumar <30999665+kumarjeet@users.noreply.github.com>
2017-09-21 23:16:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = "New Value";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft += 1;
+    marginLeft = marginLeft + 1;
     image.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
