7b3e0def557d4704a7757a4ec9cadb79b08736cb
Jeet Kumar <30999665+kumarjeet@users.noreply.github.com>
2017-09-21 23:14:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+
+element.innerHTML = "New Value";
+
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft += 1;
+    image.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+    var Interval = setInterval(moveRight, 50);
+};
